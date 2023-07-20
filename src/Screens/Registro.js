debf824aa6a2a74574eb6eea5ee5c0f1b8e0f07c
GhostHND <de_cruzn@unicah.edu>
2023-07-20 15:48:48 -0600

    a

--- src/Screens/Registro.js
+++ src/Screens/Registro.js
@@ -1,8 +1,9 @@
-import { View, Text, TouchableOpacity, ScrollView, Alert, StyleSheet, TextInput } from "react-native";
+import { View, Text, TouchableOpacity, ScrollView, Alert, StyleSheet, TextInput, Image } from "react-native";
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { COLORS } from "../constants/theme";
 import { MaterialIcons } from "@expo/vector-icons";
+import * as ImagePicker from 'expo-image-picker';
 
 const Registro = ({ navigation }) => {
   const [nombre, setNombre] = useState("");
@@ -10,6 +11,7 @@ const Registro = ({ navigation }) => {
   const [rubro, setRubro] = useState("");
   const [telefono, setTelefono] = useState("");
   const [paginaWebRedSocial, setPaginaWebRedSocial] = useState("");
+  const [image, setImage] = useState(null);
 
   const handleSubmit = () => {
     // Aquí puedes realizar alguna acción con los datos ingresados, como enviarlos a un servidor, almacenarlos en una base de datos, etc.
@@ -20,14 +22,35 @@ const Registro = ({ navigation }) => {
       Rubro: rubro,
       Teléfono: telefono,
       "Página web o red social": paginaWebRedSocial,
+      "Foto del negocio": image,
     };
     Alert.alert("Datos ingresados:", JSON.stringify(datos));
+  };
+
+  const pickImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
   };
 
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollContainer}>
         <Text style={styles.title}>Registrar Negocio</Text>
+        <TouchableOpacity onPress={pickImage} style={styles.imagePickerButton}>
+          {image ? (
+            <Image source={{ uri: image }} style={styles.imagePreview} />
+          ) : (
+            <MaterialIcons name="add-a-photo" size={30} color={COLORS.primary} />
+          )}
+        </TouchableOpacity>
         <TextInput
           style={styles.input}
           placeholder="Nombre"
@@ -79,7 +102,21 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "bold",
     marginBottom: 20,
-    textAlign:"center"
+  },
+  imagePickerButton: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: COLORS.lightGray,
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    alignSelf: "center",
+    marginBottom: 20,
+  },
+  imagePreview: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
   },
   input: {
     height: 40,
