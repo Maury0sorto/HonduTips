e37c436bf4ba696f53c99f71592a2b5bbe872785
Maury0sorto <mauricioylanumero28@gmail.com>
2023-07-20 21:27:10 -0600

    fefef

--- src/Screens/Ciudades/Danli/Banca/BancosDanli.js
+++ src/Screens/Ciudades/Danli/Banca/BancosDanli.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { View, Text, Image, TouchableHighlight, StyleSheet, ScrollView,TouchableOpacity } from 'react-native';
 import { SelectList } from 'react-native-dropdown-select-list';
 import { Ionicons } from '@expo/vector-icons';
-
+import { useNavigation } from '@react-navigation/native';
 
 import AtlantidaDanli from './BancosDanli/AtlantidaDanli';
 import FicohsaDanli from './BancosDanli/FicohsaDanli';
@@ -312,20 +312,23 @@ const MyScreen = () => {
     (button) =>
       selectedOption === 'Recomendados' || button.text.includes(selectedOption)
   );
-  
+
+  const navigation = useNavigation();
+  const handleImagePress = () => {
+    // Aquí puedes agregar la lógica que desees ejecutar cuando se presione la imagen
+    navigation.navigate('Registra tu Empresa');
+  };
 
   return (
     <View style={styles.container}>
       {mostrarSelectList && (
         <View style={styles.container2}>
-          <SelectList
-            setSelected={(val) => setSelectedOption(val)}
-            data={data2}
-            save="value"
-            placeholder="Recomendados"
-            noDataText="Banco no disponible"
-            searchPlaceholder="Busca tu banco"
-          />
+         <TouchableOpacity onPress={handleImagePress}>
+        <Image
+          source={{ uri: 'https://i.imgur.com/7YTTkEO.png' }} // Reemplaza con la URL de tu imagen
+          style={styles.image}
+        />
+      </TouchableOpacity>
         </View>
       )}
 
@@ -367,7 +370,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   container2: {
-    backgroundColor: '#ffff',
+    backgroundColor: 'transparent',
     borderRadius: 20,
     paddingHorizontal: 8,
     paddingVertical: 8,
@@ -441,6 +444,12 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold',
     
+  },
+  image: {
+    width: 362,
+    height: 76,
+   marginLeft: -35, // Alinea la imagen a la izquierda dentro del contenedor
+    // Otros estilos que desees aplicar a la imagen
   },
 });
 
